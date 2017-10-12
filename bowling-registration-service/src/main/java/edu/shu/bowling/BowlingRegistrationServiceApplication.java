694dc9dbeebae657ffd8008abc64f56b0c185afc
Jignesh Togadiya <togadiyaj@mail.sacredheart.edu>
2017-10-12 01:30:09 -0400

    Added registration service

---
+++ bowling-registration-service/src/main/java/edu/shu/bowling/BowlingRegistrationServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.shu.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingRegistrationServiceApplication.class, args);
+	}
+}
---
+++ bowling-registration-service/src/test/java/edu/shu/bowling/BowlingRegistrationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.shu.bowling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BowlingRegistrationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
