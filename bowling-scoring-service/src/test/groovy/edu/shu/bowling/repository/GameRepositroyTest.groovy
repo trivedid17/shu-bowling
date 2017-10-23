ce44d97ec95bb122523802e6a9ca6de392600a13
togadiyaj <togadiyaj@mail.sacredheart.edu>
2017-10-22 22:36:03 -0400

    Added Game repository test for scoring

---
+++ bowling-scoring-service/src/test/groovy/edu/shu/bowling/repository/GameRepositroyTest.groovy
@@ -0,0 +1,27 @@
+package edu.shu.bowling.repository
+
+import edu.shu.bowling.GameRepository
+import edu.shu.bowling.model.Game
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import spock.lang.Specification
+
+@DataJpaTest
+class GameRepositroyTest extends Specification {
+    @Autowired
+    private GameRepository repository
+
+
+    def "Save game record in database"() {
+
+        given: "a game object"
+            def game = new Game()
+                game.setLaneId(1)
+                game.setStartTime(new Date())
+
+        when: "game is saved"
+            def result = repository.saveAndFlush(game)
+        then: "gameid in result should be null"
+        result.gameId != null
+    }
+}
