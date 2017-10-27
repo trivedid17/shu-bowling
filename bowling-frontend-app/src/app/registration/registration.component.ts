f7134716a9e9e8d5edead5f4f0abf29ad203c0f2
Mayank Patel <patelm25@mail.sacredheart.edu>
2017-10-27 02:45:28 -0400

    Added frontend project

--- bowling-frontend-app/src/app/registration/registration.component.ts
+++ bowling-frontend-app/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { RegistrationService } from '../service/registration.service';
+import { UserAccount} from './user-account';
 
 
 @Component({
@@ -9,21 +10,36 @@ import { HttpClient} from '@angular/common/http';
 })
 export class RegistrationComponent implements OnInit {
 
-  firstName : string;
-  lastName : string;
-  birthDate : Date;
-  email: string;
-  phoneNumber: string;
-  userId : string;
-  password: string;
+  userAccount: UserAccount;
 
   // Inject HttpClient into your component or service.
-  constructor(private http: HttpClient) { }
+  constructor(private registrationService: RegistrationService) { }
 
   ngOnInit(): void {
+    this.userAccount = new UserAccount();
+    // Make the HTTP request:
+   /* this.http.get('/assets/data/items.json').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.name = data['name'];
+        this.email = data['email'];
+        this.phoneNumber = data['phoneNumber'];
+      },
+      // Errors will call this callback instead:
+      err => {
+        console.log('Something went wrong!');
+      }
+    );  */
+
 
   }
 
+  submit(): void{
+    this.registrationService.register(this.userAccount)
+  }
+
+  clear(): void{
+    this.userAccount = new UserAccount();
+  }
 }
 
 
