963565b7c6fad680e3202ad316d187a1ed276d6c
togadiyaj <togadiyaj@mail.sacredheart.edu>
2017-11-03 16:16:20 -0400

    Added Configuration Server

---
+++ bowling-config-server/src/test/java/edu/shu/bowling/config/BowlingConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.shu.bowling.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BowlingConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
